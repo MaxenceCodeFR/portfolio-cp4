be1c95369a9c0d3496771054c65d311b27d3839f
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-02-01 18:14:09 +0100

    crud qui fonctionne

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,17 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Project;
-use App\Form\Project1Type;
+use App\Form\Project2Type;
 use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/')]
+
+#[Route('/project')]
 class ProjectController extends AbstractController
 {
-    #[Route('/', name: 'project_index', methods: ['GET'])]
+    #[Route('/', name: 'app_project_index', methods: ['GET'])]
     public function index(ProjectRepository $projectRepository): Response
     {
         return $this->render('project/index.html.twig', [
@@ -21,11 +22,11 @@ class ProjectController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'project_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_project_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ProjectRepository $projectRepository): Response
     {
         $project = new Project();
-        $form = $this->createForm(Project1Type::class, $project);
+        $form = $this->createForm(Project2Type::class, $project);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -44,13 +45,13 @@ class ProjectController extends AbstractController
     #[Route('/{id}/edit', name: 'app_project_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Project $project, ProjectRepository $projectRepository): Response
     {
-        $form = $this->createForm(Project1Type::class, $project);
+        $form = $this->createForm(Project2Type::class, $project);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $projectRepository->save($project, true);
 
-            return $this->redirectToRoute('project_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_project_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('project/edit.html.twig', [
@@ -59,13 +60,13 @@ class ProjectController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'project_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_project_delete', methods: ['POST'])]
     public function delete(Request $request, Project $project, ProjectRepository $projectRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$project->getId(), $request->request->get('_token'))) {
             $projectRepository->remove($project, true);
         }
 
-        return $this->redirectToRoute('project_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_project_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/SinglePageController.php
+++ src/Controller/SinglePageController.php
@@ -2,37 +2,41 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[route('/', name: "portfolio")]
+
+#[Route('/', name: "portfolio")]
 class SinglePageController extends AbstractController
 {
-    #[route('/', name: "portfolio")]
+    #[Route('/', name: "portfolio")]
     public function index(): Response
     {
         return $this->render('base.html.twig');
     }
-    #[route('/', name: "links")]
+
     public function links(): Response
     {
         return $this->render('links.html.twig');
     }
-    #[route('/', name: "symfonytyped")]
-    public function symfonyUXTyped(){
+
+    public function symfonyUXTyped(): Response
+    {
 
         return $this->render('uxtyped.html.twig');
 }
-    #[route('/', name: 'presentation')]
+
     public function content(): Response
     {
         return $this->render('presentation.html.twig');
     }
-    #[route('/', name: 'hardskills')]
+
     public function hardSkills(): Response
     {
         return  $this->render('hardskills.html.twig');
     }
+
 
 }
