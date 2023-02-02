9fa01d379b8bfd57300ec9fd24d613e551543b59
MaxenceCodeFR <maxence.hattabi@gmail.com>
2023-02-02 16:17:00 +0100

    o

--- src/Controller/SinglePageController.php
+++ src/Controller/SinglePageController.php
@@ -3,8 +3,14 @@
 namespace App\Controller;
 
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -17,6 +23,27 @@ class SinglePageController extends AbstractController
         return $this->render('base.html.twig');
     }
 
+    public function contact(Request $request, MailerInterface $mailer, ContactRepository $contactRepository): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->save($contact, true);
+
+            $email = (new Email())
+                //->from($this->getParameter('mailer_from'))
+                ->from('jacquis@gmail.com&')
+                ->to('wilder@wildcodeschool.fr')
+                ->subject('Une nouvelle série vient d\'être publiée !')
+                ->html($this->renderView('newContactEmail.html.twig', ['contact' => $contact]));
+
+            $mailer->send($email);
+        }
+        return  $this->render('contact.html.twig',[
+            'form'=>$form
+        ]);
+    }
     public function links(): Response
     {
         return $this->render('links.html.twig');
@@ -37,6 +64,8 @@ class SinglePageController extends AbstractController
     {
         return  $this->render('hardskills.html.twig');
     }
+
+
 
 
 }
